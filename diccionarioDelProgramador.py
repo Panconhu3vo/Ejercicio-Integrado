2d1b02c45073e510c23f356004d4a0619936cfd4
Juan Yañez Aravena <149894926+Panconhu3vo@users.noreply.github.com>
2024-10-11 23:22:37 -0300

    Creando base del Diccionario
    Agregue la estructura del diccionario principal, además cree la base del menú y las variables necesarias para su funcionamiento.

--- diccionarioDelProgramador.py
+++ diccionarioDelProgramador.py
@@ -1,8 +1,28 @@
 # Diccionario:
-
+'''
+Estructura del diccionario.
+diccionario = {
+    "terminos,": {"definicion": "definicion en español", "traducción": "definicion en español"}
+}
+'''
+diccionario = {
+    "termino": {"definicion": "", "traduccion": ""}, # "termino" = Palabra o Metodo a trducir, el valor es el diccionario con las dos definiciones 
+    "termino": {"definicion": "", "traduccion": ""},
+    "termino": {"definicion": "", "traduccion": ""}
+}
+# ---
+# Zona de Variables
+menu = ''' 
+1: Agregar un término
+2: Eliminar un término
+3: Buscar traducción de un término
+4: Mostrar todos los términos
+5: Acerca De
+6: Salir
+''' # Varibale Del Menu
+opc = 0 # Variable de Opcion
 
 # ---
-
 # Agregar Termino():
 
 
@@ -22,3 +42,13 @@
 
 
 # Menu:
+while True:
+    
+    if opc == 1:
+        print("A")
+    # elif opc == 2:
+    # elif opc == 3:
+    # elif opc == 4:
+    # elif opc == 5:
+    # elif opc == 6:
+    # else:
