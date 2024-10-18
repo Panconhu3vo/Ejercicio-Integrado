05e6912baea521a9110c05b340764b7db87e678e
Juan Yañez Aravena <149894926+Panconhu3vo@users.noreply.github.com>
2024-10-18 12:24:57 -0300

    Nuevo Formato De Trabajo
    He separado el diccionario en un script aparte, así será mas fácil manipular los datos de este.

---
+++ diccionario.py
@@ -0,0 +1,31 @@
+# Diccionario:
+'''
+
+Estructura del diccionario.
+diccionario = {
+    "terminos,": {"definicion": "definicion en español", "traducción": "definicion en español"}
+}
+
+'''
+diccionario = { 
+    "append": {
+        "definicion": "Agrega un elemento al final de una lista.", 
+        "traduccion": "Adds an element to the end of a list."
+    },
+    "pop": {
+        "definicion": "Elimina y devuelve el último elemento de una lista.", 
+        "traduccion": "Removes and returns the last element of a list."
+    },
+    "len": {
+        "definicion": "Devuelve el número de elementos en una lista, cadena u otro objeto iterable.", 
+        "traduccion": "Returns the number of elements in a list, string, or other iterable."
+    },
+    "split": {
+        "definicion": "Divide una cadena en una lista de subcadenas según un delimitador.", 
+        "traduccion": "Splits a string into a list of substrings based on a delimiter."
+    },
+    "sort": {
+        "definicion": "Ordena los elementos de una lista en orden ascendente o descendente.", 
+        "traduccion": "Sorts the elements of a list in ascending or descending order."
+    }
+}
